d57f550092c9683868e25b18096ff8aaef4a252e
CIL PC3 <CIL PC3@CIL-PC5.cilzambia.com>
2022-05-16 15:44:52 +0200

    App setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Screen } from './components/screen'
+function App() {
+  return (
+    <div className="App">
+       <Screen/> 
+    </div>
+  );
+}
+
+export default App;
